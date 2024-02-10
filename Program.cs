9177cfe41b160c35f5cb83bd5c94fc8ffcd7a238
William Farmer <willetfarm@yahoo.com>
2024-02-10 18:23:57 +0000

    Prompt user for first and last names; print them to console window

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello everybody");
+
+Console.WriteLine("Please enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name: ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Hello " + firstName + " " + lastName);
